9568e04ec0721a1e1ff0a9ca66b45f94d36be23e
thandazilegcisa <thandazilegcisa@gmail.com>
2023-07-23 04:25:00 +0200

    added-motion-interactions

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -15,19 +15,15 @@ function runLoader() {
   function upadateLoader() {
     if (initialCounter === 100) return;
 
-    if (initialCounter < 100) {
+    initialCounter += Math.floor(Math.random() * 10) + 1;
+
+    if (initialCounter > 95) {
+      initialCounter = 100;
+      mainContent.style.display = "block";
+    } else {
       mainContent.style.display = "none";
     }
 
-    initialCounter += Math.floor(Math.random() * 10) + 1;
-
-    if (initialCounter > 100) {
-      initialCounter = 100;
-
-      if (initialCounter === 100) {
-        mainContent.style.display = "block";
-      }
-    }
     const delay = Math.floor(Math.random() * 325) + 50;
 
     overlayContainer.style.width = `${initialCounter}vw`;
@@ -37,8 +33,11 @@ function runLoader() {
 
   upadateLoader();
 }
+/* runLoader(); */
 
-runLoader();
+window.addEventListener("load", runLoader(), function () {
+  mainContent.style.display = "block";
+});
 
 // Overylay Animations : //
 
@@ -55,7 +54,7 @@ tl.to(overlayContainer, {
   })
   .to(overlayBlack, 0, {
     display: "none",
-    ease: "power3.inOut",
+    ease: "power2.easeIn",
   });
 
 // Text Animations : //
@@ -79,3 +78,83 @@ textSpan.forEach((e) => {
     e.style.color = "#676B67";
   });
 });
+
+// ! To Do: Add motion to text using gsap
+
+const textElement = document.querySelectorAll(".letterElement");
+const textAnimTl = gsap.timeline();
+
+textAnimTl.to(textElement, {
+  delay: 5,
+  transform: "translateY(0%)",
+  duration: 1,
+  stagger: 0.05,
+  ease: "power2.easeIn",
+});
+
+const navElement = document.querySelectorAll(".nav-element");
+gsap.to(navElement, {
+  delay: 5.5,
+  transform: "translateY(0%)",
+  duration: 0.5,
+  stagger: 0.05,
+  ease: "power2.easeIn",
+});
+
+const skillsItem = document.querySelectorAll(".skills-list-item");
+gsap.to(skillsItem, {
+  delay: 6,
+  opacity: 1,
+  duration: 1,
+  ease: "power2.easIn",
+});
+
+const coorContainer = document.querySelector(".coordinates-container");
+gsap.to(coorContainer, {
+  delay: 6,
+  opacity: 1,
+  duration: 1,
+  ease: "power2.easeIn",
+});
+
+/* Smooth Scroll Navigation */
+
+const navParent = document.querySelector("#navigation");
+console.log(navParent);
+navParent.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav-link")) {
+    let moveToLocation = e.target.getAttribute("href");
+    document
+      .querySelector(moveToLocation)
+      .scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+/* Interaction Observer Implementation: */
+
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  gsap.to(entry.target, {
+    delay: 0.25,
+    opacity: 1,
+    transform: "translateY(0%)",
+    duration: 1.45,
+    ease: "power2.easeIn",
+  });
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.45,
+  rootMargin: "168px",
+});
+
+let targetSection = document.querySelector(".about-section");
+/* targetSection.style.opacity = 0;
+targetSection.style.transform = "translateY(15%)"; */
+sectionObserver.observe(targetSection);
